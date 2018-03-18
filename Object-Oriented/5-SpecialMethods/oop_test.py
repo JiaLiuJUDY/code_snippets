3094660d8a675d28d81422589e3f7a2f319acc7f
Judy Liu <33583237+JiaLiuJUDY@users.noreply.github.com>
2018-03-17 23:03:27 -0500

    Update oop_test.py

--- Object-Oriented/5-SpecialMethods/oop_test.py
+++ Object-Oriented/5-SpecialMethods/oop_test.py
@@ -1,9 +1,8 @@
-
 class Employee:
 
     raise_amt = 1.04
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last, pay): #__method__: dunder method, indicate special method
         self.first = first
         self.last = last
         self.email = first + '.' + last + '@email.com'
@@ -14,14 +13,18 @@ class Employee:
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
-
+        
+    # an unambiguous represetation of the object, it should be used to debug, log, for developer
+    # this is trying to print info which enable us to recreate the object
     def __repr__(self):
         return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
-
+    
+    # a more readable represetation, for user
     def __str__(self):
         return '{} - {}'.format(self.fullname(), self.email)
 
     def __add__(self, other):
+        # add two empolyees' salary
         return self.pay + other.pay
 
     def __len__(self):
@@ -31,6 +34,17 @@ class Employee:
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
 
-# print(emp_1 + emp_2)
+print(emp_1 + emp_2)
 
-print(len(emp_1))
+#len is also a special method
+print(len(test))
+print('test'.__len__())
+
+print(len(emp_1)) 
+print(repr(emp_1))
+print(str(emp_1))
+
+print (1+2)
+print ('a' + 'b')
+print(int.__add__(1,2))
+print(str.__add__('a', 'b'))
