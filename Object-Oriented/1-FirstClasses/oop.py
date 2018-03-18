5173c013ccb4ccf8a38cc951c68e9985e154acee
Judy Liu <33583237+JiaLiuJUDY@users.noreply.github.com>
2018-03-17 21:05:44 -0500

    Update oop.py

--- Object-Oriented/1-FirstClasses/oop.py
+++ Object-Oriented/1-FirstClasses/oop.py
@@ -1,7 +1,9 @@
 
 class Employee:
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last, pay): 
+        # this is a special method which can be treated as a constructor
+        # the first arg of this __init__ is always self
         self.first = first
         self.last = last
         self.email = first + '.' + last + '@email.com'
