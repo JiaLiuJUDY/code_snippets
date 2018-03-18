bbc61478cdbd29694aaaa1a0362bb944166255e0
Judy Liu <33583237+JiaLiuJUDY@users.noreply.github.com>
2018-03-17 23:13:43 -0500

    Update oop.py

--- Object-Oriented/6-property-decorator/oop.py
+++ Object-Oriented/6-property-decorator/oop.py
@@ -4,6 +4,7 @@ class Employee:
     def __init__(self, first, last):
         self.first = first
         self.last = last
+        # self.email = first + '.' + last + '@email.com'
 
     @property
     def email(self):
@@ -12,10 +13,24 @@ class Employee:
     @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+    
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
+        
 emp_1 = Employee('John', 'Smith')
+emp_1.first = 'Jim' # originally, full name will be fine, but email will not
+
+emp_1.fullname = 'Corey Schafer' # will get error without the setter function
 
 print(emp_1.first)
-print(emp_1.email)
+print(emp_1.email) # with @property, no need to add () after email
 print(emp_1.fullname)
